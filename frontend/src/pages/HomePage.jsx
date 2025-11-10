ddfae31b2625f29fe251508eca6acfbd6193e27f
Vivien99964 <mecserikvivien2@gmail.com>
2025-11-10 13:47:09 +0200

    Started implementing first section for adoption guide

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Section from '../components/layout/Section'
 import HeroSection from '../components/home/HeroSection';
 import ChooseUsSection from '../components/home/ChooseUsSection';
 import FeaturedPetsSection from '../components/home/FeaturedPetsSection';
+import AdoptionGuideOneSection from '../components/home/AdoptionGuideOneSection';
 
 const HomePage = () => {
 
@@ -12,7 +13,7 @@ const HomePage = () => {
             <HeroSection />
            <ChooseUsSection />
            <FeaturedPetsSection />
-           
+           <AdoptionGuideOneSection />
         </>
     )
 }

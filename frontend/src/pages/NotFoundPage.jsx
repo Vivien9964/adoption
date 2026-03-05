cd4674a9290a3946f92c52dd6ab7296690848c02
Vivien99964 <mecserikvivien2@gmail.com>
2026-03-05 14:32:38 +0200

    Progressed with not found page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -5,9 +5,39 @@ import Section from "../components/layout/Section";
 const NotFoundPage = () => {
 
     return (
-        <Section padding="normal" background="white">
+        <Section padding="normal" background="blue">
 
-            <h1>Ooops, page is not found!</h1>
+            {/* Main info card with two boxes */}
+            <div className="
+                    max-w-2xl mx-auto flex overflow-hidden rounded-3xl border-2 border-sky-200 shadow-[6px_6px_0px_#bae6fd]
+            ">
+
+                {/* Box with 404 message */}
+                <div className="p-12 flex-1 bg-sky-100 border-r-2 border-dashed border-sky-200">
+
+                    {/* 404 sign */}
+                    <div className="
+                            mb-4 font-black font-fredoka text-[clamp(80px,14vw,110px)] leading-none tracking-tighter text-sky-300/80">
+                        404
+                    </div>
+                    {/* Title */}
+                    <h2 className="mb-4 text-lg md:text-xl text-sky-800 font-bold">
+                        Ooops, this page ran off to play!
+                    </h2>
+                    {/* Description */}
+                    <p className="text-md text-gray-500">
+                        Looks like this page went on an advanture. 
+                        Don't worry, our other pages are well-behaved!
+                    </p>
+                </div>
+
+                {/* Box with icon */}
+                <div className="p-10 flex-1 bg-yellow-200">
+
+                    <h1>Text</h1>
+
+                </div>
+            </div>
 
         </Section>
     )
